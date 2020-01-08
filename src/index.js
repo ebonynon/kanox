6bb10d6f22b2e2368120d2ed2901d4b9c43fee55
ebonynon <sithumsandeepap@gmail.com>
2020-01-08 14:03:26 +0530

    First push xD

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import Header from './Header';
+import Banner_part from './sec_banner_part';
+import Cta_part from './sec_cta_part';
+import Service_part from './sec_service_part';
+import About_part from './sec_about_part'
+import Our_latest_work from './sec_our_latest_work';
+import Footer from './footer'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Header />, document.getElementById('header'));
+ReactDOM.render(<Banner_part />, document.getElementById('banner_part'));
+ReactDOM.render(<Cta_part />, document.getElementById('cta_part'));
+ReactDOM.render(<Service_part />, document.getElementById('service_part'));
+ReactDOM.render(<About_part />, document.getElementById('about_part'));
+ReactDOM.render(<Our_latest_work />, document.getElementById('our_latest_work'));
+ReactDOM.render(<Footer />, document.getElementById('footer'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
